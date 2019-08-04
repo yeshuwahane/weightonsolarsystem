1ffed243f81ac2a8ec0a6c82a34e5929d30d2ee7
yeshuwahane <53706354+yeshuwahane@users.noreply.github.com>
2019-08-04 22:05:30 +0530

    Add files via upload
    first c program

---
+++ planet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+float w,m;
+                    printf("Enter your weight :  ");
+                    scanf("%f",&w);
+            m=w/9.8;
+
+                printf("\non Mercury : %f",m*274);
+                printf("\non Vesnus :  %f",m*3.7);
+                printf("\non mars :  %f",m*8.8);
+                printf("\non Earth :  %f",m*3.71);
+                printf("\non Jupiter :  %f",m*24.92);
+                printf("\non Saturm :  %f",m*10.44);
+                printf("\non Neptune  :  %f",m*11.15);
+                printf("\non Pluto : %f",m*0.58);
+                printf("\non Moon :  %f",m*1.62);
+
+          return 0;
+        }
